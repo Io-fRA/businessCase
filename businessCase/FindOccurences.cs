9d814a33f7fb282c2173f53499bf336baa78fcb0
Io-fRA <142305816+Io-fRA@users.noreply.github.com>
2024-12-16 15:35:47 +0100

    Cleaning repository

--- businessCase/FindOccurences.cs
+++ businessCase/FindOccurences.cs
@@ -2,6 +2,10 @@
 
 class FindOccurences
 {
+    private const string GitHubApiBaseUrl = "https://api.github.com";
+    private const string PathToRepo= "lodash/lodash";
+    private const string GitHubToken = "githutApiToken";
+    
     static void Main(string[] args)
     {
         
