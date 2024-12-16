b759214ddab243fd4f3c16b43dec92847ead58a6
Io-fRA <142305816+Io-fRA@users.noreply.github.com>
2024-12-16 23:44:27 +0100

    Using tree endpoint enables less API calls

--- businessCase/FindOccurences.cs
+++ businessCase/FindOccurences.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel;
-using System.Net.Http.Headers;
+﻿using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 
@@ -8,8 +7,9 @@ namespace businessCase;
 class FindOccurences
 {
     
-    static string githubApiToken = "API_TOKEN_GITHUB";
-    static string baseRepoUrl = "https://api.github.com/repos/lodash/lodash/contents";
+    static string githubApiToken = "GITHUB_API_TOKEN";
+    static string baseRepoUrl = "https://api.github.com/repos/lodash/lodash/";
+    private static string gettingTreeShaPath = "git/refs/heads/main";
     static List<string> pathOfJsAndTsFiles = new List<string>();
     private static HttpClient client;
     private static Dictionary<char, int> letterOccurrences = new Dictionary<char, int>();
@@ -22,30 +22,15 @@ class FindOccurences
         
         try
         {
-            await AssessContentUnderUrl(baseRepoUrl);
+            // Using tree endpoint uses less API calls
+            await RetrievingAllFilesUsingTree();
+            //await RetrievingAllFilesRecursively(baseRepoUrl + "contents/");
             Console.WriteLine($"There are {pathOfJsAndTsFiles.Count} .js/.ts files");
             
-            // parsing each js/ts file and contains each occurence of letter
-            foreach (var jsOrTsPath in pathOfJsAndTsFiles)
-            {
-                Console.WriteLine($"{jsOrTsPath}");
-                GitHubContent fileInfo = await GetPathContent(jsOrTsPath, new GitHubContent());
-                var decodedContent = Encoding.UTF8.GetString(Convert.FromBase64String(fileInfo.content));
-                foreach (char c in decodedContent)
-                {
-                    if (char.IsLetter(c))
-                    {
-                        if (letterOccurrences.ContainsKey(char.ToLower(c)))
-                        {
-                            letterOccurrences[char.ToLower(c)]++;
-                        }
-                        else
-                        {
-                            letterOccurrences[c] = 1;
-                        }
-                    }
-                }
-            }
+            // Parsing each js/ts file and counting occurrences of each letter
+            await ParsingJsAndTsFilesToCountLetter();
+            
+            // Sorting letter count
             var sortedDictionary = letterOccurrences
                 .OrderByDescending(pair => pair.Value)
                 .ToList();
@@ -54,12 +39,54 @@ class FindOccurences
             {
                 Console.WriteLine($"Key: {pair.Key}, Value: {pair.Value}");
             }
+
         }
         catch (HttpRequestException ex)
         {
             Console.WriteLine($"Request error: {ex.Message}");
         }
         
+    }
+
+    private static async Task ParsingJsAndTsFilesToCountLetter()
+    {
+        foreach (var jsOrTsPath in pathOfJsAndTsFiles)
+        {
+            //Console.WriteLine($"{jsOrTsPath}");
+            GitHubContent fileInfo = await GetPathContent(jsOrTsPath, new GitHubContent());
+            var decodedContent = Encoding.UTF8.GetString(Convert.FromBase64String(fileInfo.content));
+            foreach (char c in decodedContent)
+            {
+                if (char.IsLetter(c))
+                {
+                    if (letterOccurrences.ContainsKey(char.ToLower(c)))
+                    {
+                        letterOccurrences[char.ToLower(c)]++;
+                    }
+                    else
+                    {
+                        letterOccurrences[c] = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    private static async Task RetrievingAllFilesUsingTree()
+    {
+        // retrieving the sha of the github repository tree to minimize calls to the API
+        GitHubRef gitHubRef = await GetPathContent(baseRepoUrl + gettingTreeShaPath, new GitHubRef());
+        string treeSha = gitHubRef.GitHubObject.Sha;
+            
+        GitHubTree tree = await GetPathContent(baseRepoUrl + "git/trees/" + treeSha + "?recursive=1", new GitHubTree());
+        foreach (GitHubTreeObject treeObject in tree.tree)
+        {
+            if (treeObject.type == "blob" &&
+                (treeObject.path.EndsWith(".js") || treeObject.path.EndsWith(".ts")))
+            {
+                pathOfJsAndTsFiles.Add(baseRepoUrl + "contents/" + treeObject.path);
+            }
+        }
     }
     
     private static void CreateHttpClient()
@@ -78,7 +105,7 @@ class FindOccurences
     private static async Task<T> GetPathContent<T>(string path, T returnedType)
     {
         // Make the GET request
-        Console.WriteLine("Getting path content for path: " + path);
+        //Console.WriteLine("Getting path content for path: " + path);
         HttpResponseMessage response = await client.GetAsync(path);
         response.EnsureSuccessStatusCode();
         var json = await response.Content.ReadAsStringAsync();
@@ -94,7 +121,7 @@ class FindOccurences
         }
     }
 
-    private static async Task AssessContentUnderUrl(string url)
+    private static async Task RetrievingAllFilesRecursively(string url)
     {
         List<GitHubContent> gitHubContents = await GetPathContent(url, new List<GitHubContent>() );
             
@@ -103,7 +130,7 @@ class FindOccurences
             if (content.type == "dir")
             {
                 // It is a directory, recursive loop to check for js/ts files
-                await AssessContentUnderUrl(content.url);
+                await RetrievingAllFilesRecursively(content.url);
             }
             else if (content.type == "file")
             {
--- businessCase/GitHubContent.cs
+++ businessCase/GitHubContent.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace businessCase;
 
 public class GitHubContent
@@ -7,4 +9,27 @@ public class GitHubContent
     public string name  { get; set; }
     public string url  { get; set; }
     public string content  { get; set; }
+}
+
+public class GitHubRef
+{
+    [JsonPropertyName("object")]
+    public GitHubObject GitHubObject { get; set; }
+}
+
+public class GitHubObject
+{
+    [JsonPropertyName("sha")]
+    public string Sha { get; set; }
+}
+
+public class GitHubTree
+{
+    public List<GitHubTreeObject> tree  { get; set; }
+}
+
+public class GitHubTreeObject
+{
+    public string path  { get; set; }
+    public string type  { get; set; }
 }
