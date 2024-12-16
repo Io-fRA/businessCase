506fc6daf8db289fe4826357dea84b027e1c95aa
Io-fRA <142305816+Io-fRA@users.noreply.github.com>
2024-12-16 15:35:59 +0100

    Recursively parsing the repository to discover every js or ts file

---
+++ businessCase/GitHubContent.cs
@@ -0,0 +1,10 @@
+namespace businessCase;
+
+public class GitHubContent
+{
+    public string type  { get; set; }
+    public string path  { get; set; }
+    public string name  { get; set; }
+    public string url  { get; set; }
+    public string content  { get; set; }
+}
